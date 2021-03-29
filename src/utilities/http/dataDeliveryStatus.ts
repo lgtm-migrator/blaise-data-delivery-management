e64aa498c5d90a366c5a7d26ef08de3b3e177d85
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-03-29 12:59:00 +0100

    New requestPromiseJsonList function to avoid repeated code

--- src/utilities/http/dataDeliveryStatus.ts
+++ src/utilities/http/dataDeliveryStatus.ts
@@ -1,4 +1,4 @@
-import {requestPromiseJson} from "./requestPromise";
+import {requestPromiseJson, requestPromiseJsonList} from "./requestPromise";
 import {DataDeliveryBatchData, DataDeliveryFileStatus} from "../../../Interfaces";
 
 type getBatchInfoListResponse = [boolean, DataDeliveryFileStatus[]];
@@ -6,53 +6,31 @@ type getAllBatchesResponse = [boolean, DataDeliveryBatchData[]];
 type getStatusDescriptionsResponse = [boolean, any[]];
 
 function getAllBatches(): Promise<getAllBatchesResponse> {
-    let list: DataDeliveryBatchData[] = [];
     console.log("Call to getAllBatches");
     const url = "/api/batch";
 
     return new Promise((resolve: (object: getAllBatchesResponse) => void) => {
-        requestPromiseJson("GET", url).then(([status, data]) => {
-            console.log(`Response from get All Batches: Status ${status}, data list length ${data.length}`);
-            if (status === 200) {
-                if (!Array.isArray(data)) {
-                    resolve([false, list]);
-                }
-                list = data;
-                resolve([true, list]);
-            } else if (status === 404) {
-                resolve([true, list]);
-            } else {
-                resolve([false, list]);
-            }
+        requestPromiseJsonList("GET", url).then(([success, data]) => {
+            console.log(`Response from get Batch Info ${(success ? "successful" : "failed")}, data list length ${data.length}`);
+            resolve([success, data]);
         }).catch((error: Error) => {
             console.error(`Response from get All Batches Failed: Error ${error}`);
-            resolve([false, list]);
+            resolve([false, []]);
         });
     });
 }
 
 function getBatchInfo(batchName: string): Promise<getBatchInfoListResponse> {
-    let list: DataDeliveryFileStatus[] = [];
     console.log("Call to getBatchInfo");
-        const url = `/api/batch/${batchName}`;
+    const url = `/api/batch/${batchName}`;
 
     return new Promise((resolve: (object: getBatchInfoListResponse) => void) => {
-        requestPromiseJson("GET", url).then(([status, data]) => {
-            console.log(`Response from get Batch Info Status ${status}, data list length ${data.length}`);
-            if (status === 200) {
-                if (!Array.isArray(data)) {
-                    resolve([false, list]);
-                }
-                list = data;
-                resolve([true, list]);
-            } else if (status === 404) {
-                resolve([true, list]);
-            } else {
-                resolve([false, list]);
-            }
+        requestPromiseJsonList("GET", url).then(([success, data]) => {
+            console.log(`Response from get Batch Info ${(success ? "successful" : "failed")}, data list length ${data.length}`);
+            resolve([success, data]);
         }).catch((error: Error) => {
             console.error(`Response from get Batch Info Failed: Error ${error}`);
-            resolve([false, list]);
+            resolve([false, []]);
         });
     });
 }
