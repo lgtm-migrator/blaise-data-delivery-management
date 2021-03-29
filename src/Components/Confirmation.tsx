e1770bd4e50997fe51563a1259f67be8f0f80c6d
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-03-29 17:26:34 +0100

    Trigger DD tests

--- src/Components/Confirmation.tsx
+++ src/Components/Confirmation.tsx
@@ -4,27 +4,32 @@ import {ONSButton, ONSPanel} from "blaise-design-system-react-components";
 import {sendDataDeliveryRequest} from "../utilities/http";
 
 function Confirmation(): ReactElement {
+    const [formError, setFormError] = useState<string>("");
     const [redirect, setRedirect] = useState<boolean>(false);
-    const [confirm, setConfirm] = useState<boolean>(false);
+    const [confirm, setConfirm] = useState<boolean | null>(null);
     const [loading, setLoading] = useState<boolean>(false);
     const [message, setMessage] = useState<string>("");
     const history = useHistory();
 
     async function confirmOption() {
-        setLoading(true);
-        setMessage("");
-
+        if (confirm === null) {
+            setFormError("Select an answer");
+            return;
+        }
         if (!confirm) {
             history.push("/");
             return;
         }
+
+        setLoading(true);
+        setMessage("");
 
         const success = await sendDataDeliveryRequest();
 
         setLoading(false);
 
         if (!success) {
-            setMessage("Failed to trigger Data Delivery");
+            setMessage("Failed to trigger Data Delivery.");
             setRedirect(true);
             return;
         }
@@ -56,46 +61,18 @@ function Confirmation(): ReactElement {
                 </ONSPanel>
             }
 
-            <form onSubmit={() => confirmOption()} className="u-mt-m">
-                <fieldset className="fieldset">
-                    <legend className="fieldset__legend">
-                    </legend>
-                    <div className="radios__items">
-
-                        <p className="radios__item">
-                        <span className="radio">
-                        <input
-                            type="radio"
-                            id="confirm-overwrite"
-                            className="radio__input js-radio "
-                            value="True"
-                            name="confirm-delete"
-                            aria-label="No"
-                            onChange={() => setConfirm(true)}
-                        />
-                        <label className="radio__label " htmlFor="confirm-overwrite">
-                            Yes, trigger Data Delivery
-                        </label>
-                    </span></p>
-                        <br/>
-                        <p className="radios__item">
-                        <span className="radio">
-                        <input
-                            type="radio"
-                            id="cancel-keep"
-                            className="radio__input js-radio "
-                            value="False"
-                            name="confirm-delete"
-                            aria-label="Yes"
-                            onChange={() => setConfirm(false)}
-                        />
-                        <label className="radio__label " htmlFor="cancel-keep">
-                            No, do not trigger Data Delivery
-                        </label>
-                    </span>
-                        </p>
-                    </div>
-                </fieldset>
+            <form className="u-mt-m">
+                {
+                    formError === "" ?
+                        confirmDeleteRadios(setConfirm)
+                        :
+                        <ONSPanel status={"error"}>
+                            <p className="panel__error">
+                                <strong>{formError}</strong>
+                            </p>
+                            {confirmDeleteRadios(setConfirm)}
+                        </ONSPanel>
+                }
 
                 <br/>
                 <ONSButton
@@ -113,6 +90,50 @@ function Confirmation(): ReactElement {
                 }
             </form>
         </>
+    );
+}
+
+function confirmDeleteRadios(setConfirm: (value: (((prevState: (boolean | null)) => (boolean | null)) | boolean | null)) => void) {
+    return (
+        <fieldset className="fieldset">
+            <legend className="fieldset__legend">
+            </legend>
+            <div className="radios__items">
+
+                <p className="radios__item">
+                        <span className="radio">
+                        <input
+                            type="radio"
+                            id="confirm-overwrite"
+                            className="radio__input js-radio "
+                            value="True"
+                            name="confirm-delete"
+                            aria-label="No"
+                            onChange={() => setConfirm(true)}
+                        />
+                        <label className="radio__label " htmlFor="confirm-overwrite">
+                            Yes, trigger Data Delivery
+                        </label>
+                    </span></p>
+                <br/>
+                <p className="radios__item">
+                        <span className="radio">
+                        <input
+                            type="radio"
+                            id="cancel-keep"
+                            className="radio__input js-radio "
+                            value="False"
+                            name="confirm-delete"
+                            aria-label="Yes"
+                            onChange={() => setConfirm(false)}
+                        />
+                        <label className="radio__label " htmlFor="cancel-keep">
+                            No, do not trigger Data Delivery
+                        </label>
+                    </span>
+                </p>
+            </div>
+        </fieldset>
     );
 }
 
---
+++ src/features/step_definitions/trigger_data_delivery.test.tsx
@@ -0,0 +1,234 @@
+// React
+import React from "react";
+// Test modules
+import {defineFeature, loadFeature} from "jest-cucumber";
+import {act, cleanup, fireEvent, render, screen, waitFor} from "@testing-library/react";
+import {createMemoryHistory} from "history";
+import App from "../../App";
+import {Router} from "react-router";
+import "@testing-library/jest-dom";
+// Mock elements
+import flushPromises from "../../tests/utils";
+import {mock_fetch_requests} from "./functions";
+import {BatchInfoList, BatchList, StatusDescriptions} from "./mock_objects";
+
+
+// Load in feature details from .feature file
+const feature = loadFeature(
+    "./src/features/trigger_data_delivery.feature",
+    {tagFilter: "not @server and not @integration"}
+);
+
+const mock_server_responses = (url: string) => {
+    console.log(url);
+    if (url.includes("/api/trigger")) {
+        return Promise.resolve({
+            status: 200,
+            json: () => Promise.resolve("completed"),
+        });
+    } else if (url.includes("/api/batch")) {
+        return Promise.resolve({
+            status: 200,
+            json: () => Promise.resolve(BatchList),
+        });
+    } else if (url.includes("/api/state/descriptions")) {
+        return Promise.resolve({
+            status: 200,
+            json: () => Promise.resolve(StatusDescriptions),
+        });
+    }
+};
+
+const mock_server_responses_trigger_fails = (url: string) => {
+    console.log(url);
+    if (url.includes("/api/trigger")) {
+        return Promise.resolve({
+            status: 500,
+            json: () => Promise.resolve("failed"),
+        });
+    } else if (url.includes("/api/batch")) {
+        return Promise.resolve({
+            status: 200,
+            json: () => Promise.resolve(BatchList),
+        });
+    } else if (url.includes("/api/state/descriptions")) {
+        return Promise.resolve({
+            status: 200,
+            json: () => Promise.resolve(StatusDescriptions),
+        });
+    }
+};
+
+defineFeature(feature, test => {
+    afterEach(() => {
+        jest.clearAllMocks();
+        cleanup();
+        jest.resetModules();
+    });
+
+    beforeEach(() => {
+        cleanup();
+    });
+
+    test("Trigger data delivery", ({given, when, then, and}) => {
+        given("I have launched the Data Delivery Management", async () => {
+            mock_fetch_requests(mock_server_responses);
+            const history = createMemoryHistory();
+            render(
+                <Router history={history}>
+                    <App/>
+                </Router>
+            );
+            await act(async () => {
+                await flushPromises();
+            });
+        });
+
+        when("I click the link 'Trigger Data Delivery'", async () => {
+            await act(async () => {
+                fireEvent.click(screen.getByText(/Trigger Data Delivery/));
+            });
+
+        });
+
+        and("I confirm I want to trigger Data Delivery", async () => {
+            await act(async () => {
+                fireEvent.click(screen.getByText(/Yes, trigger Data Delivery/));
+                await flushPromises();
+                fireEvent.click(screen.getByText(/Continue/));
+                await flushPromises();
+            });
+        });
+
+        then("Data Delivery will be triggered", async () => {
+            await act(async () => {
+                await flushPromises();
+            });
+        });
+
+        and("I will be redirect to the homepage with a message saying it had been triggered successfully", async () => {
+            await waitFor((() => {
+                expect(screen.getByText(/Data delivery runs/i)).toBeDefined();
+                expect(screen.getByText(/Triggered Data Delivery successfully, It may take a few minutes for the run to appear in the table below./i)).toBeDefined();
+            }));
+        });
+    });
+
+    test("Trigger data delivery fails", ({given, when, then, and}) => {
+        given("I have launched the Data Delivery Management", async () => {
+            mock_fetch_requests(mock_server_responses_trigger_fails);
+            const history = createMemoryHistory();
+            render(
+                <Router history={history}>
+                    <App/>
+                </Router>
+            );
+            await act(async () => {
+                await flushPromises();
+            });
+        });
+
+        when("I click the link 'Trigger Data Delivery'", async () => {
+            await act(async () => {
+                fireEvent.click(screen.getByText(/Trigger Data Delivery/));
+            });
+        });
+
+        and("I confirm I want to trigger Data Delivery", async () => {
+            await act(async () => {
+                fireEvent.click(screen.getByText(/Yes, trigger Data Delivery/));
+                await flushPromises();
+                fireEvent.click(screen.getByText(/Continue/));
+                await flushPromises();
+            });
+        });
+
+        then("Data Delivery will be triggered", async () => {
+            await act(async () => {
+                await flushPromises();
+            });
+        });
+
+        and("Data Delivery fails", async () => {
+            await act(async () => {
+                await flushPromises();
+            });
+        });
+
+        and("I will be redirect to the homepage with a message saying the the trigger had failed", async () => {
+            await waitFor((() => {
+                expect(screen.getByText(/Data delivery runs/i)).toBeDefined();
+                expect(screen.getByText(/Failed to trigger Data Delivery./i)).toBeDefined();
+            }));
+        });
+    });
+
+    test("Cancel Trigger data delivery", ({given, when, then, and}) => {
+        given("I have been presented with a confirmation to trigger data delivery", async () => {
+            mock_fetch_requests(mock_server_responses_trigger_fails);
+            const history = createMemoryHistory();
+            render(
+                <Router history={history}>
+                    <App/>
+                </Router>
+            );
+            await act(async () => {
+                await flushPromises();
+                fireEvent.click(screen.getByText(/Trigger Data Delivery/));
+                await flushPromises();
+            });
+            await waitFor((() => {
+                expect(screen.getByText(/Are you sure you want to trigger Data Delivery?/i)).toBeDefined();
+            }));
+        });
+
+        when("I confirm that I do NOT want to proceed", async () => {
+            await act(async () => {
+                fireEvent.click(screen.getByText(/No, do not trigger Data Delivery/));
+                await flushPromises();
+                fireEvent.click(screen.getByText(/Continue/));
+                await flushPromises();
+            });
+        });
+
+        then("I am returned to the landing page", async () => {
+            await waitFor((() => {
+                expect(screen.getByText(/Data delivery runs/i)).toBeDefined();
+            }));
+        });
+    });
+
+    test("Don't select an option", ({given, when, then}) => {
+        given("I have been presented with a confirmation to trigger data delivery", async () => {
+            mock_fetch_requests(mock_server_responses_trigger_fails);
+            const history = createMemoryHistory();
+            render(
+                <Router history={history}>
+                    <App/>
+                </Router>
+            );
+            await act(async () => {
+                await flushPromises();
+                fireEvent.click(screen.getByText(/Trigger Data Delivery/));
+                await flushPromises();
+            });
+            await waitFor((() => {
+                expect(screen.getByText(/Are you sure you want to trigger Data Delivery?/i)).toBeDefined();
+            }));
+        });
+
+        when("I select confirm without choosing an option", async () => {
+            await act(async () => {
+                fireEvent.click(screen.getByText(/Continue/));
+                await flushPromises();
+            });
+        });
+
+        then("I am presented with message telling me to choose an option", async () => {
+            await waitFor((() => {
+                expect(screen.queryByText(/Data delivery runs/i)).not.toBeInTheDocument();
+                expect(screen.getByText(/Select an answer/i)).toBeDefined();
+            }));
+        });
+    });
+});
