b238965d9d8edd23334cbab9de965ad9e4698db7
enigma-cloud <torreskristian77@gmail.com>
2022-10-21 11:47:29 +0100

    refactor: convert to use axios

--- src/utilities/http/requestPromise.ts
+++ src/utilities/http/requestPromise.ts
@@ -1,3 +1,5 @@
+import axios, { Method } from "axios";
+
 type PromiseResponse = [number, any];
 
 function requestPromiseJson(method: string, url: string, body: any = null): Promise<PromiseResponse> {
@@ -21,38 +23,33 @@ function requestPromiseJson(method: string, url: string, body: any = null): Prom
     });
 }
 
-type PromiseResponseList = [boolean, []];
+type PromiseResponseList = [boolean, any[]];
 
-function requestPromiseJsonList(method: string, url: string, body: any = null): Promise<PromiseResponseList> {
-    return new Promise((resolve: (object: PromiseResponseList) => void, reject: (error: string) => void) => {
-        fetch(url, {
-            "method": method,
-            "body": body
-        })
-            .then(async response => {
-                response.json().then(
-                    data => {
-                        if (response.status === 200) {
-                            if (!Array.isArray(data)) {
-                                resolve([false, []]);
-                            }
-                            resolve([true, data]);
-                        } else if (response.status === 404) {
-                            resolve([true, data]);
-                        } else {
-                            resolve([false, []]);
-                        }
-                    }
-                ).catch((error) => {
-                    console.log(`Failed to read JSON from response, Error: ${error}`);
-                    resolve([false, []]);
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                reject(err);
-            });
-    });
+async function requestPromiseJsonList(method: Method, url: string, body: any = null): Promise<PromiseResponseList> {
+    try {
+        const response = await axios({
+            url: url,
+            method: method,
+            data: body,
+            validateStatus: () => true,
+        });
+        
+        const data = response.data; 
+    
+        if (response.status === 200) {
+            if (!Array.isArray(data)) {
+                return [false, []];
+            }
+            return [true, data];
+        } else if (response.status === 404) {
+            return [true, data];
+        } else {
+            return [false, []];
+        }
+    } catch (error) {
+        console.log(error);
+        throw error;        
+    }
 }
 
 export {requestPromiseJson, requestPromiseJsonList};
